740b092e3464fc6641649a9b4bbddd2f31a9c838
meeturglider <64744351+meeturglider@users.noreply.github.com>
2023-03-21 13:17:49 +0530

    Create vars.tf

---
+++ three-tier-architecure/terraform/vars.tf
@@ -0,0 +1,29 @@
+variable "env" {
+  description = "The environment to deploy"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "The cidr range for vpc"
+  type        = string
+}
+
+variable "pub_subnet_b_cidr" {
+  description = "The cidr range for public subnet b"
+  type        = string
+}
+
+variable "pub_subnet_c_cidr" {
+  description = "The cidr range for public subnet c"
+  type        = string
+}
+
+variable "prv_subnet_b_cidr" {
+  description = "The cidr range for private subnet b"
+  type        = string
+}
+
+variable "prv_subnet_c_cidr" {
+  description = "The cidr range for private subnet c"
+  type        = string
+}
